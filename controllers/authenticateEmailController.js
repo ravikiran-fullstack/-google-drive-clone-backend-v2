86aef5d08d64057b88a9943c78290e6bd6ac4716
kiran-code <ravikiransjce.code@gmail.com>
2021-02-04 23:01:41 +0530

    If the email is verified no need to verify it again, expiration check code is pending

--- controllers/authenticateEmailController.js
+++ controllers/authenticateEmailController.js
@@ -6,6 +6,10 @@ export const authEmail = async (req, res) => {
   const result = await RegisterUser.findOne({username: req.params.username});
   console.log(result);
   
+  if (result.emailVerified) { 
+    return res.status(302).redirect(`${process.env.frontEndUrl}/home`);
+  }
+
   if (result.randomKey === req.params.randomKey) {
     const dbResult = await RegisterUser.updateOne( { username: result.username },{ emailVerified: true});
     res.status(302).redirect(`${process.env.frontEndUrl}/home`);
